925de32774ba22704ba8187eef4bddb93accc0d4
FairyMental <radu-adrian.marcu@students.plymouth.ac.uk>
2017-01-04 12:03:54 +0000

    Pushing commands, implementation will be added soon.

---
+++ src/commands/vehicleManagement/RemoveAllocationRecord.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package commands.vehicleManagement;
+
+import commands.interfaces.ICommandBehavior;
+import models.AllocationRecord;
+import models.Car;
+import models.Insurance;
+
+/**
+ *
+ * @author FairyMental
+ */
+public class RemoveAllocationRecord implements ICommandBehavior{
+    private Car car;
+    private AllocationRecord record;
+    
+    
+    public RemoveAllocationRecord(AllocationRecord _record)
+    {
+        record = _record;
+    }
+    
+    @Override
+    public boolean executeCommand()
+    {
+        car.removeAllocationRecord(record);
+        return true;
+    }
+    @Override
+    public boolean undoCommand()
+    {
+        car.addAllocationRecord(record);
+        return true;
+    }
+}
