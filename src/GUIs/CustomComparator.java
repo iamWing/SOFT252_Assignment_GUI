c3a3fc74ee780dee4b8b2789b0e98d8fa8cd95b1
Mike <notmike@notmike.co.uk>
2017-01-14 15:59:23 +0000

    Javadoc

--- src/GUIs/CustomComparator.java
+++ src/GUIs/CustomComparator.java
@@ -5,10 +5,18 @@ import java.util.Comparator;
 import models.Car;
 
 /**
+ * Custom comparator for sorting objects in lists.
  *
  * @author Admin
  */
 public class CustomComparator implements Comparator<Object>{
+    /**
+     * Compares objects and returns correct order.
+     * 
+     * @param object1 First object.
+     * @param object2 Second object.
+     * @return comparison
+     */
     public int compare(Object object1, Object object2) {
         return object1.toString().compareTo(object2.toString());
     }
